803928aa3d9f5c2179af9af9b1ee5815398be4f5
Ross Golder <ross@golder.org>
2017-08-17 09:57:13 +0700

    De-duplication, simplify and optimise.

--- odoo-xmlrpc-config.js
+++ odoo-xmlrpc-config.js
@@ -5,6 +5,19 @@ module.exports = function (RED) {
         this.db = n.db;
         this.username = n.username;
         this.password = n.password;
+
+        this.connect = function(callback) {
+            var Odoo = require('odoo-xmlrpc');
+            var odoo_inst = new Odoo({
+                url: this.url,
+                db: this.db,
+                username: this.username,
+                password: this.password,
+            });
+            odoo_inst.connect(function (err) {
+                return callback(err, odoo_inst);
+            });
+        };
     }
     RED.nodes.registerType("odoo-xmlrpc-config", OdooXMLRPCConfigNode);
 }
--- unlink-node.js
+++ unlink-node.js
@@ -1,43 +1,32 @@
 module.exports = function (RED) {
+    var handle_error = function(err, node) {
+        node.log(err.body);
+        node.status({fill: "red", shape: "dot", text: err.message});
+        node.error(err.message);
+    };
+
     function OdooXMLRPCUnlinkNode(config) {
         RED.nodes.createNode(this, config);
-        var Odoo = require('odoo-xmlrpc');
-        var url = require('url');
         this.host = RED.nodes.getNode(config.host);
         var node = this;
-        try {
-            node.on('input', function (msg) {
-                var odoo_inst = new Odoo({
-                    url: this.host.url,
-                    db: this.host.db,
-                    username: this.host.username,
-                    password: this.host.password,
-                });
-                odoo_inst.connect(function (err) {
+
+        node.on('input', function (msg) {
+            this.host.connect(function(err, odoo_inst) {
+                if (err) {
+                    return handle_error(err, node);
+                }
+
+                var ids = msg.payload;
+                //node.log('Deleting ' + ids.length + ' records for model "' + config.model + '"...');
+                odoo_inst.execute_kw(config.model, 'unlink', [[ids]], function (err, value) {
                     if (err) {
-                        node.log(err.body);
-                        node.status({fill: "red", shape: "dot", text: err.message});
-                        node.error(err.message);
-                        return;
+                        return handle_error(err, node);
                     }
-                    var ids = msg.payload;
-                    node.log('Deleting ' + ids.length + ' records for model "' + config.model + '"...');
-                    odoo_inst.execute_kw(config.model, 'unlink', [[ids]], function (err, value) {
-                        if (err) {
-                            node.log(err.body);
-                            node.status({fill: "red", shape: "dot", text: err.message});
-                            node.error(err.message);
-                            return;
-                        }
-                        msg.payload = value;
-                        node.send(msg);
-                    });
+                    msg.payload = value;
+                    node.send(msg);
                 });
             });
-        } catch (err) {
-            node.status({fill: "red", shape: "dot", text: err.message});
-            node.error(err.message);
-        }
+        });
     }
     RED.nodes.registerType("odoo-xmlrpc-unlink", OdooXMLRPCUnlinkNode);
 };
