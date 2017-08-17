6fff510c140132456221ae6fbbd5cb5c008d267d
Ross Golder <ross@golder.org>
2017-08-17 11:16:45 +0700

    Add 'create', 'update' and 'search-read' nodes.

---
+++ search-read-node.js
@@ -0,0 +1,36 @@
+module.exports = function (RED) {
+    var handle_error = function(err, node) {
+        node.log(err.body);
+        node.status({fill: "red", shape: "dot", text: err.message});
+        node.error(err.message);
+    };
+
+    function OdooXMLRPCSearchReadNode(config) {
+        RED.nodes.createNode(this, config);
+        this.host = RED.nodes.getNode(config.host);
+        var node = this;
+
+        node.on('input', function (msg) {
+            node.status({});
+            this.host.connect(function(err, odoo_inst) {
+                if (err) {
+                    return handle_error(err, node);
+                }
+
+                var inParams = [];
+                inParams.push([]);
+                var params = [];
+                params.push(inParams);
+                //node.log('Search-reading for model "' + config.model + '"...');
+                odoo_inst.execute_kw(config.model, 'search-read', params, function (err, value) {
+                    if (err) {
+                        return handle_error(err, node);
+                    }
+                    msg.payload = value;
+                    node.send(msg);
+                });
+            });
+        });
+    }
+    RED.nodes.registerType("odoo-xmlrpc-search-read", OdooXMLRPCSearchReadNode);
+};
