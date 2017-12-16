d4b2fe1b6ea0200134454b3f46494a3d91d3e353
Ahmad Benmrad <batikhsouri@gmail.com>
2017-12-16 22:03:03 +0100

    Adding the limit and offset features in the search & search-read nodes (which were documented but not implemented). Failing if msg.filters is defined but is not an array

--- search-read-node.js
+++ search-read-node.js
@@ -1,3 +1,11 @@
+function isDefinedValue(v){
+  return !(v == null || typeof v === 'undefined');
+}
+
+function isUInt(v){
+  return typeof v === 'number' && Math.floor(v) === v && v >= 0;
+}
+
 module.exports = function (RED) {
     var handle_error = function(err, node) {
         node.log(err.body);
@@ -17,8 +25,20 @@ module.exports = function (RED) {
                     return handle_error(err, node);
                 }
 
+                var offset = msg.offset;
+                if (isDefinedValue(offset) && !isUInt(offset)){
+                  return handle_error(new Error('When offset is provided, it must be a positive integer number'), node);
+                }
+                var limit = msg.limit;
+                if (isDefinedValue(limit) && !isUInt(limit)){
+                  return handle_error(new Error('When limit is provided, it must be a positive integer number'), node);
+                }
+
                 var inParams;
-                if (msg.filters && (Array.isArray(msg.filters) && msg.filters.length > 0)){
+                if (msg.filters){
+                  if (!Array.isArray(msg.filters)){
+                    return handle_error(new Error('When filters is provided, it must be an array'), node);
+                  }
                   inParams = msg.filters;
                 } else {
                   inParams = [];
@@ -30,6 +50,15 @@ module.exports = function (RED) {
                 odoo_inst.execute_kw(config.model, 'search_read', params, function (err, value) {
                     if (err) {
                         return handle_error(err, node);
+                    }
+
+                    if (isDefinedValue(offset)){
+                      //Jump the x first elements (where x has the value of the "offset" variable)
+                      value = value.slice(offset);
+                    }
+                    if (isDefinedValue(limit)){
+                      //Limit the length of the value array to x elements (where x has the value of the "limit" variable)
+                      value = value.slice(0, limit);
                     }
                     msg.payload = value;
                     node.send(msg);
